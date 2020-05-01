f8472c97e2ca89e4afc12e515ae5fef8ea51050d
Mateus Oliveira <mateusoli.viana@gmail.com>
2020-05-01 19:18:28 -0300

    create frame

--- gameProject/src/controller/Main.java
+++ gameProject/src/controller/Main.java
@@ -1,12 +1,27 @@
 package controller;
 
+import view.JFGame;
+
 /**
  * @author Mateus - git hub: https://github.com/mateusoli98
  */
 public class Main {
 
     public static void main(String[] args) {
-      
+        try {
+            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
+                if ("Nimbus".equals(info.getName())) {
+                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
+                    break;
+                }
+            }
+        } catch (ClassNotFoundException | InstantiationException | IllegalAccessException | javax.swing.UnsupportedLookAndFeelException ex) {
+            java.util.logging.Logger.getLogger(JFGame.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        }
+
+        java.awt.EventQueue.invokeLater(() -> {
+            new JFGame().setVisible(true);
+        });
     }
-    
+
 }
