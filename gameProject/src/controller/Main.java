4997302861f48d4e94b373d71cb9963f44d90082
Mateus Oliveira <mateusoli.viana@gmail.com>
2020-05-01 17:18:38 -0300

    init project

---
+++ gameProject/src/controller/Main.java
@@ -0,0 +1,12 @@
+package controller;
+
+/**
+ * @author Mateus - git hub: https://github.com/mateusoli98
+ */
+public class Main {
+
+    public static void main(String[] args) {
+      
+    }
+    
+}
